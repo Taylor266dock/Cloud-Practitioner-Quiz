d7187301a2a8283620767b5664c22461643ba4b8
tayldock <taylor.dockerty@outlook.com>
2023-04-14 16:41:21 +0100

    Ironed out some bugs
    Questions were repeating
    
    Also cleaned up most warnings in code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 import xlrd
-from random import randint, shuffle
+from random import randint
 
 import os
 
@@ -9,19 +9,17 @@ import os
 Q = []
 A = {}
 
-#############################
-#	Excel grid explained	#
-#############################
-#	a1 = cell_value(0,0)	#
-#	a2 = cell_value(1,0)	#
-#	b3 = cell_value(2,1)	#
-#############################
+
+# Excel grid explained
+# a1 = cell_value(0,0)
+# a2 = cell_value(1,0)
+# b3 = cell_value(2,1)
 
 
 name = 'Questions'		# Excel sheet name
 
 #############################
-#	Read from Excel 		#
+# Read from Excel 		#
 #############################
 
 
@@ -30,7 +28,7 @@ sheet = book_read.sheet_by_name("Q&A")								# Choose Excel sheet
 i = 1																# Choose start column/row (to be used for looping)
 while True:
     try:
-        f = sheet.cell_value(i,2)									# Choose start column/row (row,column)
+        f = sheet.cell_value(i, 2)									# Choose start column/row (row,column)
         Q.append(f)													# Add selected cell to list
         i += 1														# Increment i, change cell
     except IndexError:												# When cell is empty
@@ -39,15 +37,14 @@ while True:
 
 # Read answers
 for i in range(len(Q)):
-    A[Q[i]]=sheet.cell_value(i+1,3)
-
+    A[Q[i]] = sheet.cell_value(i+1, 3)
 
 
 # Initial values
 tries = 0
 streak = 0
-streakrecord = 0
-q = randint(0,len(Q)-1)
+streakRecord = 0
+q = randint(0, len(Q)-1)
 stillPlaying = True
 answer = ""
 noQuestions = 0
@@ -55,25 +52,41 @@ correctQ = 0
 
 
 print('+-------->')
-print('| Quiz time: %s' %(name))
+print('| Quiz time: %s' % name)
 print('+---------------------------------+')
 print("| Commands: 'quit' 'exit'         |")
 print('+---------------------------------+')
 print('| 1st question;')
 
+# Condensed the question code into a re-usable function
+
+
+def current_scores():
+    percentQ = ((correctQ / noQuestions) * 100)
+    print(f"Number of Questions: {noQuestions}")
+    print(f"Number of Correct Answers: {correctQ}")
+    print(f"Your percentage score: {percentQ}%")
+
+
 def question():
+    # global keyword to allow changes to be made to the variable
     global answer
     global stillPlaying
-    print('\n| %s' % (Q[q]))
+    print('\n| %s' % (Q[random.randint(1, len(Q) - 1)]))
+    # using the random function to generate answers from full bank
+    # we need to refine the answers to be more relevant
+    # we also need to use some if statements to stop duplicate answers
     opt1 = A[Q[random.randint(1, 510)]]
-    opt2 = A[Q[random.randrange(1, 511)]]
-    opt3 = A[Q[random.randrange(1, 511)]]
+    opt2 = A[Q[random.randrange(1, 510)]]
+    opt3 = A[Q[random.randrange(1, 510)]]
     options_list = [opt1, opt2, opt3, A[Q[q]]]
+    # using random shuffle function to mix up the answers otherwise the correct answer would be A every time
     random.shuffle(options_list)
     print(f"A: {options_list[0]}")
     print(f"B: {options_list[1]}")
     print(f"C: {options_list[2]}")
     print(f"D: {options_list[3]}")
+    # Get user to input answer
     answer = input("| Answer: ")
     if answer == "A":
         answer = options_list[0]
@@ -89,50 +102,43 @@ def question():
     else:
         print("Not a valid answer please try again.")
         question()
-    print(answer)
 
 # Starting questions loop
-while stillPlaying == True:
+
+
+while stillPlaying is True:
 
     question()
-    print(answer)
     tries += 1
     noQuestions += 1
 
-
-    if answer != A[Q[q]]:
-        print('| Wrong answer!')
-    elif answer == A[Q[q]]:
+    if answer == A[Q[q]]:
+        os.system('cls')
         correctQ += 1
-        percentQ = correctQ / noQuestions * 100
-        print('| Correct! try: %i' %(tries))
-        print(noQuestions)
-        print(correctQ)
-        print(f"{percentQ}%")
-        if tries <= 1:
-            streak += 1
-        if streak >= 5:
-            print("| Nice streak! %i in a row!" %(streak))
-            if streak > streakrecord:
-                print('+-------------------------->')
-                print("| New streak record! %i" %(streak))
-                print('+--------------------------------->')
+        print('| Correct! try: %i' % tries)
+        current_scores()
+        # if tries <= 1:
+        #    streak += 1
+        # if streak >= 5:
+        #    print("| Nice streak! %i in a row!" % streak)
+        #    if streak > streakRecord:
+        #        print('+-------------------------->')
+        #        print("| New streak record! %i" % streak)
+        #        print('+--------------------------------->')
         print('| Next Question;')
-        q = randint(0,len(Q)-1)
+
         tries = 0
-
-
-
+    elif answer != A[Q[q]]:
+        percentQ = ((correctQ / noQuestions) * 100)
+        print('| Wrong answer!')
+        current_scores()
 
     elif tries > 5:
-        print('| You failed 5 times, correct answer: %s' %(A[Q[q]]))
+        print('| You failed 5 times, correct answer: %s' % (A[Q[q]]))
         tries = 0
-        if streak > streakrecord:
-            streakrecord = streak
-        #		print '+-------------------------->'
-        #		print "| New streak record! %i" %(streak)
-        #		print '+--------------------------------->'
+        if streak > streakRecord:
+            streakRecord = streak
+        # print '+-------------------------->'
+        # print "| New streak record! %i" %(streak)
+        # print '+--------------------------------->'
         streak = 0
-        q = randint(0,len(Q)-1)
-        print('| Next Question;')
-
